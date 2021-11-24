d2eb8fabc28ae7f481b965957803f458d3f0d2ce
edgardo90 <edgardo9000@gmail.com>
2021-11-24 15:45:54 -0300

    Base de datos terminado (finishid database version 1.5)

--- api/src/models/Diet.js
+++ api/src/models/Diet.js
@@ -4,15 +4,15 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('diet', {
-    id:{ // este es mi id
-      type: DataTypes.UUID,
-      defaultValue: DataTypes.UUIDV4,
-      allowNull: false, // pongo en false para que sea obligatorio poner algo
-      primaryKey: true, 
-    },
+    // id:{ // este es mi id
+    //   type: DataTypes.UUID,
+    //   defaultValue: DataTypes.UUIDV4,
+    //   allowNull: false, // pongo en false para que sea obligatorio poner algo
+    //   primaryKey: true, 
+    // },
     title: { // este es mi nombre
       type: DataTypes.STRING,
-      allowNull: false,
+      allowNull: true,
     },
 
   });
